df52368083b9d176d2fc38245c2d2d6dd0e37aba
NiallMcGurk <niallmcgurk530@gmail.com>
2025-09-04 17:03:08 +0100

    Implemented Logic inside of Create Account prepping for backend save

--- ProjectOrryxPrime/ReactTest/src/components/CreateAccount.tsx
+++ ProjectOrryxPrime/ReactTest/src/components/CreateAccount.tsx
@@ -1,4 +1,43 @@
+import { useState } from "react";
+
+interface AccountDetails {
+  username: string;
+  email: string;
+  password: string;
+  repeatPassword: string;
+}
+
 function CreateAccount() {
+  const [accountDetails, setloginDetails] = useState<AccountDetails>({
+    username: "",
+    email: "",
+    password: "",
+    repeatPassword: "",
+  });
+
+  const handlerSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (accountDetails.password !== accountDetails.repeatPassword) {
+      alert("Passwords do not match.");
+      return;
+    }
+
+    console.log(accountDetails);
+
+    const response = await fetch("http://http://localhost:5173/createAccount", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        accountDetails,
+      }),
+    });
+
+    const data = await response.json();
+    console.log("Sent to C#" + data);
+  };
   return (
     <section className="vh-100">
       <div className="container h-100">
@@ -11,116 +50,129 @@ function CreateAccount() {
                     <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
                       Sign up
                     </p>
-
-                    <form className="mx-1 mx-md-4">
-                      <div className="d-flex flex-row align-items-center mb-4">
-                        <i className="fas fa-user fa-lg me-3 fa-fw"></i>
-                        <div
-                          data-mdb-input-init
-                          className="form-outline flex-fill mb-0"
-                        >
-                          <input
-                            type="text"
-                            id="form3Example1c"
-                            className="form-control"
-                          />
-                          <label
-                            className="form-label"
-                            htmlFor="form3Example1c"
+                    <>
+                      <form onSubmit={handlerSubmit}>
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <i className="fas fa-user fa-lg me-3 fa-fw"></i>
+                          <div
+                            data-mdb-input-init
+                            className="form-outline flex-fill mb-0"
                           >
-                            Your Name
-                          </label>
+                            <input
+                              type="text"
+                              id="form3Example1c"
+                              className="form-control"
+                              value={accountDetails.username}
+                              onChange={(event) =>
+                                setloginDetails({
+                                  ...accountDetails,
+                                  username: event.target.value,
+                                })
+                              }
+                            />
+                            <label
+                              className="form-label"
+                              htmlFor="form3Example1c"
+                            >
+                              Your Name
+                            </label>
+                          </div>
                         </div>
-                      </div>
 
-                      <div className="d-flex flex-row align-items-center mb-4">
-                        <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
-                        <div
-                          data-mdb-input-init
-                          className="form-outline flex-fill mb-0"
-                        >
-                          <input
-                            type="email"
-                            id="form3Example3c"
-                            className="form-control"
-                          />
-                          <label
-                            className="form-label"
-                            htmlFor="form3Example3c"
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                          <div
+                            data-mdb-input-init
+                            className="form-outline flex-fill mb-0"
                           >
-                            Your Email
-                          </label>
+                            <input
+                              type="email"
+                              id="form3Example3c"
+                              className="form-control"
+                              value={accountDetails.email}
+                              onChange={(event) =>
+                                setloginDetails({
+                                  ...accountDetails,
+                                  email: event.target.value,
+                                })
+                              }
+                            />
+                            <label
+                              className="form-label"
+                              htmlFor="form3Example3c"
+                            >
+                              Your Email
+                            </label>
+                          </div>
                         </div>
-                      </div>
 
-                      <div className="d-flex flex-row align-items-center mb-4">
-                        <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
-                        <div
-                          data-mdb-input-init
-                          className="form-outline flex-fill mb-0"
-                        >
-                          <input
-                            type="password"
-                            id="form3Example4c"
-                            className="form-control"
-                          />
-                          <label
-                            className="form-label"
-                            htmlFor="form3Example4c"
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
+                          <div
+                            data-mdb-input-init
+                            className="form-outline flex-fill mb-0"
                           >
-                            Password
-                          </label>
+                            <input
+                              type="password"
+                              id="form3Example4c"
+                              className="form-control"
+                              value={accountDetails.password}
+                              onChange={(event) =>
+                                setloginDetails({
+                                  ...accountDetails,
+                                  password: event.target.value,
+                                })
+                              }
+                            />
+                            <label
+                              className="form-label"
+                              htmlFor="form3Example4c"
+                            >
+                              Password
+                            </label>
+                          </div>
                         </div>
-                      </div>
 
-                      <div className="d-flex flex-row align-items-center mb-4">
-                        <i className="fas fa-key fa-lg me-3 fa-fw"></i>
-                        <div
-                          data-mdb-input-init
-                          className="form-outline flex-fill mb-0"
-                        >
-                          <input
-                            type="password"
-                            id="form3Example4cd"
-                            className="form-control"
-                          />
-                          <label
-                            className="form-label"
-                            htmlFor="form3Example4cd"
+                        <div className="d-flex flex-row align-items-center mb-4">
+                          <i className="fas fa-key fa-lg me-3 fa-fw"></i>
+                          <div
+                            data-mdb-input-init
+                            className="form-outline flex-fill mb-0"
                           >
-                            Repeat your password
-                          </label>
+                            <input
+                              type="password"
+                              id="form3Example4cd"
+                              className="form-control"
+                              value={accountDetails.repeatPassword}
+                              onChange={(event) =>
+                                setloginDetails({
+                                  ...accountDetails,
+                                  repeatPassword: event.target.value,
+                                })
+                              }
+                            />
+                            <label
+                              className="form-label"
+                              htmlFor="form3Example4cd"
+                            >
+                              Repeat your password
+                            </label>
+                          </div>
                         </div>
-                      </div>
-
-                      <div className="form-check d-flex justify-content-center mb-5">
-                        <input
-                          className="form-check-input me-2"
-                          type="checkbox"
-                          value=""
-                          id="form2Example3c"
-                        />
-                        <label
-                          className="form-check-label"
-                          htmlFor="form2Example3"
-                        >
-                          I agree all statements in{" "}
-                          <a href="#!">Terms of service</a>
-                        </label>
-                      </div>
-
-                      <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                        <button
-                          type="button"
-                          data-mdb-button-init
-                          data-mdb-ripple-init
-                          className="btn btn-primary btn-lg"
-                        >
-                          Register
-                        </button>
-                      </div>
-                    </form>
+                        <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                          <button
+                            type="submit"
+                            data-mdb-button-init
+                            data-mdb-ripple-init
+                            className="btn btn-primary btn-lg"
+                          >
+                            Register
+                          </button>
+                        </div>
+                      </form>
+                    </>
                   </div>
+
                   <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2"></div>
                 </div>
               </div>
